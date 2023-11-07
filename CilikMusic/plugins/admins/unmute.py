9ac1f63bdbb733fcb65ee842e45bb55282d23bbe
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-07 16:31:37 +0300

    Update unmute.py

--- CilikMusic/plugins/admins/unmute.py
+++ CilikMusic/plugins/admins/unmute.py
@@ -22,7 +22,7 @@ UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
 
 
 @app.on_message(
-    filters.command(UNMUTE_COMMAND)
+    filters.command(["تكلم","اتكلم"],"")
     & filters.group
     & ~BANNED_USERS
 )
