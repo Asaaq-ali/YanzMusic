0c42c84a06a167a07cfd50cfb660324b35ff63f2
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-07 16:27:31 +0300

    Update seek.py

--- CilikMusic/plugins/admins/seek.py
+++ CilikMusic/plugins/admins/seek.py
@@ -22,7 +22,7 @@ SEEK_COMMAND = get_command("SEEK_COMMAND")
 
 
 @app.on_message(
-    filters.command(SEEK_COMMAND)
+    filters.command(["تقديم","تقدم"],"")
     & filters.group
     & ~BANNED_USERS
 )
