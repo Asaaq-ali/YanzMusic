0249b2f64f1ff93ac55d72d4fd392857cf33f825
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-07 16:20:13 +0300

    Update pause.py

--- CilikMusic/plugins/admins/pause.py
+++ CilikMusic/plugins/admins/pause.py
@@ -22,7 +22,7 @@ PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
 
 @app.on_message(
-    filters.command(PAUSE_COMMAND)
+    filters.command(["قف","مؤقتا"],"")
     & filters.group
     & ~BANNED_USERS
 )
